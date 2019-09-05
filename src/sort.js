1f7e7b34795f5da0bc91844a3e948199fba67e80
alex <alexandrk_@hotmail.com>
2019-09-05 14:27:49 -0400

    BACKLOG-10989 Add comment

--- src/sort.js
+++ src/sort.js
@@ -1,6 +1,7 @@
 function invalidSortFields(qc) {
     return qc === null || qc.sortDirection === "" || qc.sortDirection === undefined || qc.sortField === "" || qc.sortField === undefined;
 }
+// Generate sort field
 export default function(state) {
     if (invalidSortFields(state)) {
         return "";
